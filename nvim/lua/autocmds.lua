bd3acbcadc063a8f30611bd9c282a6a46cb79f4d
= <=>
2025-12-08 14:11:35 +0700

    feat: add hightlight when yank

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,12 @@
 require "nvchad.autocmds"
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("nvchad_custom_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup "highlight_yank",
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
